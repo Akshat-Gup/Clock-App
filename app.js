ce85d18a0e5df53c0b967933b72f7cead6baa3fa
Akshat-Gup <69629823+Akshat-Gup@users.noreply.github.com>
2021-01-28 19:53:40 +0800

    refactored code

--- app.js
+++ app.js
@@ -3,18 +3,9 @@ const mn = document.querySelector("#mn");
 const sc = document.querySelector("#sc");
 const digitalClock = document.querySelector(".digitalClock");
 let now, hh, mm, ss;
-const updateTime = () => {
-	const now = new Date;
-	const h = now.getHours();
-	const min = now.getMinutes();
-	const sec = now.getSeconds();
-	const html = `
-	<p>${h}:${min}:${sec}</p>
-	`;
-	digitalClock.innerHTML = html;
-};
 
 setInterval(()=>{
+	//Analog Clock Code
 	now = new Date();
 	hh = now.getHours();
 	if (hh>12) {
@@ -27,9 +18,9 @@ setInterval(()=>{
 	hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`; //The mm/12 extends the range of the hour bar by upto 30 deg (distance between 2 bars)
 	mn.style.transform = `rotateZ(${mm}deg)`;
 	sc.style.transform = `rotateZ(${ss}deg)`;
-
-},1000)
-
-
-
-setInterval(updateTime, 1000);
+	//Digital Clock Code
+	const html = `
+	<p>${hh/30}:${mm/6}:${ss/6}</p>
+	`;
+	digitalClock.innerHTML = html;
+},1000);
